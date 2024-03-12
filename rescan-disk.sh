4d0202919e8cdb55f94d4cdf28ebd999922ef156
Ông Tùng Dương <97881749+ongtungduong@users.noreply.github.com>
2024-03-12 08:48:36 +0700

    Delete rescan-disk.sh

--- rescan-disk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for DEVICE_DIR in $(ls /sys/class/scsi_device/)
-do
-    echo "Found device: ${DEVICE_DIR}"
-    echo "Bringing online"
-    echo 1 > /sys/class/scsi_device/$DEVICE_DIR/device/rescan
-done
