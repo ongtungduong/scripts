e82a5a468bc629eb7ab10e9a649b0becbde78a90
Ông Tùng Dương <97881749+ongtungduong@users.noreply.github.com>
2024-03-12 23:33:27 +0700

    Create rescan-disk.sh

---
+++ rescan-disk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Rescan Disk
+echo "-----Rescan Disk-----"
+for DEVICE_DIR in $(ls /sys/class/scsi_device/)
+do
+    echo "Found device: ${DEVICE_DIR}"
+    echo "Bringing online"
+    echo 1 > /sys/class/scsi_device/$DEVICE_DIR/device/rescan
+done
